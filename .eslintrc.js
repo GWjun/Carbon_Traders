935e3736b3eb7828999759dd3e266d9fa1ac1a3f
GWjun <kb865900@gmail.com>
2024-03-15 19:36:47 +0900

    Init gwjun's boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    '@titicaca/eslint-config-triple',
+    '@titicaca/eslint-config-triple/requiring-type-checking',
+    '@titicaca/eslint-config-triple/frontend',
+    '@titicaca/eslint-config-triple/prettier',
+  ],
+}
